4c945b66ac9ceaa6c994f373dc4caebf319a0969
Adriana Koch <akoch@oberlin.edu>
2025-04-21 10:52:54 -0400

    update script.js file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const COHORT = "2502-FTB-ET-WEB-PT";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
+const state = {
+    parties: [],
+};
+
+const partyList = document.querySelector("#partyList");
+const partyForm = document.querySelector("#partyForm");
+partyForm.addEventListener("submit", addParty);
+partyList.addEventListener("click", deleteParty);
